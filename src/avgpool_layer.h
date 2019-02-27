f73daf27afe23554e48877c26e9a14bc4fa759a7
Wan Youwen <1756429307@qq.com>
2019-02-27 11:19:04 +0800

    均值池化层 函数声明 创建初始化函数 前向传播/反向传播 cpu/gpu 函数声明

--- src/avgpool_layer.h
+++ src/avgpool_layer.h
@@ -1,3 +1,4 @@
+// 均值池化层 函数声明 创建初始化函数 前向传播/反向传播 cpu/gpu 函数声明
 #ifndef AVGPOOL_LAYER_H
 #define AVGPOOL_LAYER_H
 
@@ -6,17 +7,24 @@
 #include "layer.h"
 #include "network.h"
 
-typedef layer avgpool_layer;
+typedef layer avgpool_layer; // typedef 类型重命名
 
 image get_avgpool_image(avgpool_layer l);
+// 创建均值池化层===申请内存等
 avgpool_layer make_avgpool_layer(int batch, int w, int h, int c);
+// 变形
 void resize_avgpool_layer(avgpool_layer *l, int w, int h);
+
+// cpu实现=======
+// 均值池化层前向传播
 void forward_avgpool_layer(const avgpool_layer l, network_state state);
+// 均值池化层反向传播
 void backward_avgpool_layer(const avgpool_layer l, network_state state);
 
+// gpu实现=====
 #ifdef GPU
-void forward_avgpool_layer_gpu(avgpool_layer l, network_state state);
-void backward_avgpool_layer_gpu(avgpool_layer l, network_state state);
+void forward_avgpool_layer_gpu(avgpool_layer l, network_state state); // 前向传播
+void backward_avgpool_layer_gpu(avgpool_layer l, network_state state);// 反向传播
 #endif
 
 #endif
