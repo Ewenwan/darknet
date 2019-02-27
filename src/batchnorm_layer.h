acf9eab9542f6af580e6ee738f7f5dbdb4daf9a3
Wan Youwen <1756429307@qq.com>
2019-02-27 15:39:15 +0800

    批 归一化层 层初始化函数  cpu/gpu 前向传播 / 反向传播函数 声明

--- src/batchnorm_layer.h
+++ src/batchnorm_layer.h
@@ -1,3 +1,4 @@
+// 批 归一化层 层初始化函数  cpu/gpu 前向传播 / 反向传播函数 声明
 #ifndef BATCHNORM_LAYER_H
 #define BATCHNORM_LAYER_H
 
@@ -5,10 +6,13 @@
 #include "layer.h"
 #include "network.h"
 
-layer make_batchnorm_layer(int batch, int w, int h, int c);
+layer make_batchnorm_layer(int batch, int w, int h, int c);// 批 归一化层 层初始化函数
+
+// cpu  前向传播 / 反向传播函数 声明
 void forward_batchnorm_layer(layer l, network_state state);
 void backward_batchnorm_layer(layer l, network_state state);
 
+// gpu 前向传播 / 反向传播函数 声明
 #ifdef GPU
 void forward_batchnorm_layer_gpu(layer l, network_state state);
 void backward_batchnorm_layer_gpu(layer l, network_state state);
